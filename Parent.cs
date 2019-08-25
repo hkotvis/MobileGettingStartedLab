7ffa65025950feaaa3750d1e51dcceebc9725f4d
hkotvis <46495573+hkotvis@users.noreply.github.com>
2019-08-24 22:52:57 -0500

    Added 4 files
    App.config, lab1.csproj, parent.cs, program.cs

---
+++ Parent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    interface Parent
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        int Id { get; set; }
+        enum Program { Computer Science; Accounting, Nursing}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 namespace Lab1
 {
@@ -8,58 +8,92 @@ namespace Lab1
         {
             Console.Write("Enter a number between 3 and 9: ");
             int initial = Int32.Parse(Console.ReadLine());
-            if(initial < 3 || initial > 9)
+            if (initial < 3 || initial > 9)
             {
                 Console.WriteLine("You need to write a number between 3 and 9!");
             }
             else
             {
-
-                
                 for (int i = 1; i <= initial; i++)
                 {
-                    Console.Write(i + " ");
-                    if (initial % i == 0) Console.WriteLine();
+                    Console.Write(i + "\t");
+                    for (int j = 2; j <= initial; j++)
+                    {
+                        if (i > 0) Console.Write(i * j + "\t");
+                        else Console.Write(j + "\t");
+                    }
+                    Console.Write("\n");
                 }
-                    
-                    
-                
-                
 
-                Console.WriteLine();
-                for (int j = 1; j <= initial; j++)
+            }
+            Console.Write("Flip on 1) Horizontal, 2) Vertical, 3) Diagonal, 4) To End: ");
+            int choice= 0;
+            try
+            {
+                 choice = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.Write(e);
+            }
+
+            if (choice == 1)
                 {
-                    Console.Write(j + j + " ");
+                    for (int i = initial; i >= 1; i--)
+                    {
+                        Console.Write(i + "\t");
+                        for (int j = initial - (initial - 2); j <= initial; j++)
+                        {
+                            if (i > 0) Console.Write(i * j + "\t");
+                            else Console.Write(j + "\t");
+                        }
+                        Console.Write("\n");
+                    }
+                    for (int i = initial; i >= 1; i--)
+                    {
+                        Console.Write(i + "\t");
+                        for (int j = 2; j <= initial; j++)
+                        {
+                            if (i > 0) Console.Write(i * j + "\t");
+                            else Console.Write(j + "\t");
+                        }
+                        Console.Write("\n");
+                    }
                 }
-                Console.WriteLine();
-                for (int k = 1; k <= initial; k++)
+                else if (choice == 2)
                 {
-                    Console.Write(k + k + k + " ");
+                    for (int i = 1; i <= initial; i++)
+                    {
+                        for (int j = initial; j >= 1; j--)
+                        {
+                            if (i > 0) Console.Write(i * j + "\t");
+                            else Console.Write(j + "\t");
+                        }
+                        Console.Write("\n");
+                    }
                 }
-            }
-            Console.Write("Flip on 1) Horizon, 2) Vertical, 3) Diagonal Left, 4) Diagonal Right, 5) To End: ");
-            if(Int32.Parse(Console.ReadLine()) == 1)
-            {
-
-            }
-            else if (Int32.Parse(Console.ReadLine()) == 2)
-            {
-
-            }
-            else if (Int32.Parse(Console.ReadLine()) == 3)
-            {
-
-            }
-            else if (Int32.Parse(Console.ReadLine()) == 4)
-            {
-
-            }
-            else
-            {
-
-            }
-
-
+                else if (choice == 3)
+                { // Diagonal Left and Right are the same
+                    for (int i = initial; i >= 1; i--)
+                    {
+                        for (int j = initial; j >= 1; j--)
+                        {
+                            if (i > 0) Console.Write(i * j + "\t");
+                            else Console.Write(j + "\t");
+                        }
+                        Console.Write("\n");
+                    }
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine("You need to choose an option between 1 and 4");
+                }
+                Console.ReadLine();
+           
         }
     }
 }
